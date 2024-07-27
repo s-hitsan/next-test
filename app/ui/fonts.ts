2d035b89a15026e9233a02ba679f7acda5522a93
s-hitsan <vasyliohloserhii@gmail.com>
2024-07-27 14:40:29 +0300

    lessons progress 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Actor, Lusitana} from 'next/font/google';
+
+export const actor = Actor({ subsets: ['latin'], weight: '400' });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
